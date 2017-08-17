a5fb5a4e602548f1ee2d6d303e47d27640da1e36
denis_barabash <barabash.denis@gmail.com>
2017-08-17 21:41:12 +0300

    add conf add database

--- stack/db.js
+++ stack/db.js
@@ -3,7 +3,7 @@ var connection = mysql.createConnection({
     host     : 'us-cdbr-iron-east-05.cleardb.net',
     user     : 'bcb1b298150b3e',
     password : '2308421a', // Please change to your local db password
-    database : 'stackover' // DB name
+    database : 'heroku_d3747e56b77da10' // DB name
 });
 
 connection.connect(function(err) {
